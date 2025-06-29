98a5a8c22a2b0c87a8948217650dcf2d97307bdf
Aryan Doifode <aryandoifode16@gmail.com>
2025-06-30 00:00:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ from psycopg2.extras import RealDictCursor
 
 # PostgreSQL database connection
 def get_db():
-    return psycopg2.connect(os.getenv("DATABASE_URL"), cursor_factory=RealDictCursor)
+    db_url = os.getenv("DATABASE_URL").strip()
+    conn = psycopg2.connect(db_url, cursor_factory=RealDictCursor)
 
 def query_db(query, args=(), one=False):
     conn = get_db()
